2e2ae3d3ce0e08b75e00070124b8ca57a29d59d5
Shameer Ebrahim <shameer66ster@gmail.com>
2025-02-03 14:53:24 +0530

    controller,models and routes are set

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const dotenv = require("dotenv").config();
 const dbConnect = require("./config/dbConnect");
+const authRoutes = require("./routes/authRoutes");
 
 dbConnect();
 
@@ -10,7 +11,7 @@ const app = express();
 app.use(express.json());
 
 // Routes
-
+app.use("/api/auth", authRoutes);
 // Start the server
 const PORT = process.env.PORT || 7002;
 
